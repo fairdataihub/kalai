4bb5f92c9621e62b742219e21a805c95a35028d0
Sanjay Soundarajan <sanjay.16@live.com>
2023-10-29 12:25:56 -0700

    💄 fix: update style

--- pages/index.tsx
+++ pages/index.tsx
@@ -148,7 +148,7 @@ export default function Index() {
             <div className="flex flex-row items-center space-x-2 mt-10">
               <button
                 type="submit"
-                className=" rounded border-b-4 border-blue-700 bg-blue-500 py-2 px-4 font-bold text-white transition-all hover:border-blue-500 hover:bg-blue-400 flex items-center justify-center space-x-3 transition-all"
+                className=" rounded border-b-4 border-blue-700 bg-blue-500 py-2 px-4 font-bold text-white transition-all hover:border-blue-500 hover:bg-blue-400 flex items-center justify-center space-x-3 "
               >
                 {showSpinner && <span className={`loader mx-2`}></span>}
                 Submit
